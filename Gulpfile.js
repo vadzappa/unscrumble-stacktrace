ebcdc8c39d61808c30629e86055d465657253b67
Vadzim Zapolski <vadzim.zapolski@pipedrive.com>
2016-03-29 00:04:32 +0300

    made production bundle version with --env production

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp'),
 	browserify = require('browserify'),
 	sass = require('gulp-sass'),
-	copy = require('gulp-contrib-copy'),
+	concat = require('gulp-concat'),
+	cleanCSS = require('gulp-clean-css'),
+	production = require('gulp-environments').production,
+	uglify = require('gulp-uglify'),
+	buffer = require('vinyl-buffer'),
 	source = require('vinyl-source-stream'),
 	buildFolder = './dist/';
 
 gulp.task('copy', function() {
-	return gulp.src('./chrome-extension/**')
-		.pipe(copy())
+	return gulp.src(['./chrome-extension/**', '!./chrome-extension/scss/**/*'])
 		.pipe(gulp.dest(buildFolder));
 });
 
@@ -15,12 +18,16 @@ gulp.task('browserify', function() {
 	return browserify('./lib/extension/main')
 		.bundle()
 		.pipe(source('unscrambler.js'))
+		.pipe(buffer())
+		.pipe(production(uglify()))
 		.pipe(gulp.dest(buildFolder));
 });
 
 gulp.task('sass', function() {
 	return gulp.src('./chrome-extension/scss/**/*.scss')
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('main.css'))
+		.pipe(production(cleanCSS()))
 		.pipe(gulp.dest(buildFolder));
 });
 
