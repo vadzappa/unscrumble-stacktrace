4f120f6f3f7ed5b535e6b6f961564fc0881f29f3
Vadzim Zapolski <vadzim.zapolski@pipedrive.com>
2016-04-01 14:25:17 +0300

    ready for publishing to web store

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,18 +6,21 @@ var gulp = require('gulp'),
 	production = require('gulp-environments').production,
 	uglify = require('gulp-uglify'),
 	buffer = require('vinyl-buffer'),
+	zip = require('gulp-zip'),
 	clean = require('gulp-clean'),
 	source = require('vinyl-source-stream'),
-	buildFolder = './dist/';
+	distFolder = function() {
+		return production() ? './build/' : './dist/';
+	};
 
 gulp.task('clean', function() {
-	return gulp.src(buildFolder + '**/*', {read: false})
+	return gulp.src(distFolder() + '**/*', {read: false})
 		.pipe(clean());
 });
 
 gulp.task('copy', function() {
-	return gulp.src(['./chrome-extension/**', '!./chrome-extension/scss/**/*'])
-		.pipe(gulp.dest(buildFolder));
+	return gulp.src(['./chrome-extension/**', '!./chrome-extension/scss{,/**}'])
+		.pipe(gulp.dest(distFolder()));
 });
 
 gulp.task('script:foreground', function() {
@@ -26,7 +29,7 @@ gulp.task('script:foreground', function() {
 		.pipe(source('unscrambler.js'))
 		.pipe(buffer())
 		.pipe(production(uglify()))
-		.pipe(gulp.dest(buildFolder + 'js'));
+		.pipe(gulp.dest(distFolder() + 'js'));
 });
 
 gulp.task('script:background', function() {
@@ -35,7 +38,7 @@ gulp.task('script:background', function() {
 		.pipe(source('context.js'))
 		.pipe(buffer())
 		.pipe(production(uglify()))
-		.pipe(gulp.dest(buildFolder + 'js'));
+		.pipe(gulp.dest(distFolder() + 'js'));
 });
 
 gulp.task('sass', function() {
@@ -43,7 +46,20 @@ gulp.task('sass', function() {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('main.css'))
 		.pipe(production(cleanCSS()))
-		.pipe(gulp.dest(buildFolder + 'css'));
+		.pipe(gulp.dest(distFolder() + 'css'));
 });
 
 gulp.task('build', ['clean', 'copy', 'script:foreground', 'script:background', 'sass']);
+
+gulp.task('zip', ['clean', 'copy', 'script:foreground', 'script:background', 'sass'], function() {
+	var manifest = require('./chrome-extension/manifest'),
+		distFileName = manifest.name + ' v' + manifest.version + '.zip';
+
+	return gulp.src([distFolder() + '/**/*'])
+		.pipe(zip(distFileName))
+		.pipe(gulp.dest(distFolder()));
+});
+
+gulp.task('chrome-extension', ['clean'], function() {
+	gulp.start('zip');
+});
